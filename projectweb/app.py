d5c577e0baaf990d2e8c045d9e6190ea0ce46493
DESKTOP-F6DG3P0\HP <dragon15098@gmail.com>
2017-04-09 14:52:03 +0700

    login tiếp

--- projectweb/app.py
+++ projectweb/app.py
@@ -1,8 +1,17 @@
 from flask import *
 from flask_login import *
+from sessionuser import SessionUser
+from werkzeug.utils import *
+from models.user import User
+import mlab
 
 app = Flask(__name__)
+mlab.connect()
 
+admin_user = User()
+admin_user.useraname = 'admin'
+admin_user.password = 'admin'
+admin_user.save()
 
 @app.route('/')
 def hello_world():
@@ -12,9 +21,21 @@ def hello_world():
 def homepage():
     return render_template('homepage.html')
 
-@app.route('/login')
-def login():
-    return render_template('login.html')
+@app.route('/login', methods=["GET", "POST"])
+def login_web():
+    if request.method == "GET":
+        return render_template("login.html")
+    elif request.method == "POST":
+        user = User.objects(username=request.form["username"]).first()
+        if user and user.password == request.form["password"]:
+            session_user = SessionUser(user.id)
+            user.update(set__token=str(user.id))
+            login_user(session_user)
+            return render_template("login.html")
+        else:
+            pass
+            return redirect(url_for("homepage"))
+
 
 @app.route('/sign_up')
 def sign_up():
--- projectweb/models/user.py
+++ projectweb/models/user.py
@@ -2,7 +2,3 @@ from mongoengine import *
 class User(Document):
     username = StringField()
     password = StringField()
-    token = StringField()
-    image = FileField()
-    number = StringField()
-    address = StringField()
