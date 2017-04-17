27e37cc8025dea0804fb4a63c907a7b8c1632a72
DESKTOP-F6DG3P0\HP <dragon15098@gmail.com>
2017-04-18 01:20:18 +0700

    xong backend

--- projectweb/app.py
+++ projectweb/app.py
@@ -100,27 +100,21 @@ def sign_up_web():
             return render_template("sign_up.html")
 
 
-@app.route('/boy_page', methods=["GET", "POST"])
+@app.route('/boy_page')
 @login_required
 def boy_page():
     num = Number.objects(name="abc").first()
     temp = random.randint(0, num.numberboy)
     boy = User.objects(number=temp, gender="male").first()
-    if(request.method == "GET"):
-        return render_template('boy_page.html', description=boy.description, user=boy.image)
-    elif (request.method == "POST"):
-        num = Number.objects(name="abc").first()
-        temp = random.randint(0, num.numberboy)
-        boy = User.objects(number=temp, gender="male").first()
-        return render_template('boy_page.html', description=boy.description, user=boy.image)
-
+    print(boy.description)
+    return render_template('boy_page.html', description=boy.description, image=boy.image)
 
 @app.route('/girl_page')
 def girl_page():
     num = Number.objects(name="abc").first()
     temp = random.randint(0, num.numbergirl)
-    girl = User.objects(number=temp, gender="male").first()
-    return render_template('girl_page.html', description=girl.description)
+    girl = User.objects(number=temp, gender="female").first()
+    return render_template('girl_page.html', description=girl.description, image=girl.image)
 
 
 @app.route('/update', methods=["GET", "POST"])
